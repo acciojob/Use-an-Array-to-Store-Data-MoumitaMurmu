f1dee46a5798034fec028a5ed01eca40466d441a
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2022-12-17 03:01:50 +0530

    initial commit

--- solution.js
+++ solution.js
@@ -1,7 +1,8 @@
 
-let yourArray;
+
 // assign your array the correct value in the next line
 // your code here!
+let yourArray = ['thirty', 2007, true, 26, 'love'];
 // code end
 
 module.exports = yourArray
